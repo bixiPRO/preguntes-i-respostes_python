f4778a0aaf713883a0531b6d52d23d3ce636a627
Bixiang <bixiangzhu6666@gmail.com>
2023-12-30 22:17:07 +0100

    prova llista

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,3 +1,7 @@
 #Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
+#llista de preguntes amb la seva resposta
+preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
 
-#Mostra la puntuació de l'usuari d'acord de com ha respost la pregunta
+for i in preguntes:
+    print(i)
+#Mostra la puntuació de l'usuari d'acord de com ha respost la preguntagit
