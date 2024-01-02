0d1cb02cda63f1c6b814697d9a90050407ed0a67
Bixiang <bixiangzhu6666@gmail.com>
2024-01-02 20:30:16 +0100

    mostrar la pregunta aleatoria

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -9,8 +9,9 @@ seed = (seed * 997) % 1000
 random = (seed * 503) % 1000 / 100
 numero_pregunta = int(random * (len(llista_preguntes)))
 
-print(numero_pregunta)
-                                
-for i in llista_preguntes:
-    print(i)
-#Mostra la puntuació de l'usuari d'acord de com ha respost la preguntagit
+#mostrar pregunta
+pregunta_elegir=llista_preguntes[numero_pregunta%len(llista_preguntes)]
+pregunta=pregunta_elegir[0]
+
+print(pregunta)
+#Mostra la puntuació de l'usuari d'acord de com ha respost la pregunta
