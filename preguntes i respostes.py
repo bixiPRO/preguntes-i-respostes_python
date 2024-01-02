5b2e3ba1bbd2e19ced5dc0be4f05e5e9e450838d
Bixiang <bixiangzhu6666@gmail.com>
2024-01-02 20:07:43 +0100

    numero aleatori

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,9 +1,10 @@
+import random
 #Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
 #llista de preguntes amb la seva resposta
 llista_preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
 
 #numero aleatori
-seed=48
+seed= random.randint(0,100)
 seed = (seed * 997) % 1000
 random = (seed * 503) % 1000 / 100
 numero_pregunta = int(random * (len(llista_preguntes)))
