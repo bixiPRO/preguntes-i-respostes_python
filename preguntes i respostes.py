dd0036af6f76106c594c79652a84d9de6828bf26
Bixiang <bixiangzhu6666@gmail.com>
2024-01-02 19:33:31 +0100

    pregunta_aleatori

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,7 +1,15 @@
 #Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
 #llista de preguntes amb la seva resposta
-preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
+llista_preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
 
-for i in preguntes:
+#numero aleatori
+seed=48
+seed = (seed * 997) % 1000
+random = (seed * 503) % 1000 / 100
+numero_pregunta = int(random * (len(llista_preguntes)))
+
+print(numero_pregunta)
+                                
+for i in llista_preguntes:
     print(i)
 #Mostra la puntuació de l'usuari d'acord de com ha respost la preguntagit
