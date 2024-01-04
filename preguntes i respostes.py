4fc225c773e699c59412d1055eb66173c4192f55
Bixiang <bixiangzhu6666@gmail.com>
2024-01-04 13:57:06 +0100

    resposta incorrecte (no se perque)

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,7 +1,7 @@
 import random
 #Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
 #llista de preguntes amb la seva resposta
-llista_preguntes=[("Quina es la capital de espana"," Madrid"),("Quina es la capital de italia", "Roma")]
+llista_preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
 
 #numero aleatori
 seed= random.randint(0,100)
@@ -15,15 +15,20 @@ pregunta=pregunta_elegir[0]
 resposta=pregunta_elegir[1]
 resposta=resposta.strip()
 resposta=resposta.lower()
+print(pregunta)
 print(resposta)
 #bucle
 resposta="si"
-puntuació=0
+puntuacio=0
 
 while resposta == "si":
     print(pregunta)
     resposta_usuari=input("Resposta: ")
     resposta_usuari=resposta_usuari.strip()
     resposta_usuari=resposta_usuari.lower()
-    
+    if resposta_usuari==resposta:
+        puntuacio+=1
+        print(puntuacio)
+    else:
+        print("Resposta incorrecte!")
 #Mostra la puntuació de l'usuari d'acord de com ha respost la pregunta
