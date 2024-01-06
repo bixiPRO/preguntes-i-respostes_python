403a9fbab14a831556b4a568efe9f4cb1e190e1a
Bixiang <bixiangzhu6666@gmail.com>
2024-01-06 16:44:46 +0100

    posar tots els paisos amb la seva capital i regles de joc

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,14 +1,16 @@
 #PRE://Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
 import random
 #llista de preguntes amb la seva resposta
-llista_preguntes=[("Quina es la capital de Espana","Madrid"),("Quina es la capital de Italia", "Roma"),("Quina es la capital de Japó", "Toquio"), ("Quina es la capital de França", "Paris"), ("Quina es la capital de Russia", "Moscou"), ("Quina es la capital de Alemanya", "Berlin"), ("Quina es la capital de India", "Nova Delhi"), ("Quina es la capital de China", "Pekin"), ("Quina es la capital de Mèxic", "Ciutat de Mexic"), ("Quina es la capital de Austràlia", "Cambra") ]
+llista_preguntes=[("Quina es la capital de Algèria","Alger"), ("Quina es la capital de Kenya","Nairobi"), ("Quina es la capital de Sudan","Khartum"), ("Quina es la capital de Marroc","Rabat"), ("Quina es la capital de Gabon","Libreville"), ("Quina es la capital de Uruguai","Montevideo"), ("Quina es la capital de Perú","Lima"), ("Quina es la capital de Bolívia","Sucre"), ("Quina es la capital de Cuba","Habana"), ("Quina es la capital de Venezuela","Caracas"), ("Quina es la capital de Canadà","Ottawa"), ("Quina es la capital de Mèxic","Ciutat de Mexic"), ("Quina es la capital de Xina","Pequin"), ("Quina es la capital de Afganistan","Kabul"), ("Quina es la capital de Índia","Nova Delhi"), ("Quina es la capital de Japó","Toquio"), ("Quina es la capital de Espanya","Madrid"), ("Quina es la capital de França","Paris"), ("Quina es la capital de Romania","Bucarest"), ("Quina es la capital de Liechtenstein","Vaduz"), ("Quina es la capital de Bielorússia","Minsk"), ("Quina es la capital de Itàlia","Roma"), ("Quina es la capital de Dinamarca","Copenhaguen"), ("Quina es la capital de Austràlia","Canberra")]
 
 #bucle
 resposta_bucle="si"
 puntuacio=0
 contador_vegadeswhile=0
 
-print("No posis exentos en ninguna resposta, sinó declarara com a malament")
+print("IMPORTANT:")
+print("- La resposta ha de ser en Català")
+print("- No posis exentos en ninguna resposta, sinó declarara com a malament")
 
 while resposta_bucle == "si":
     contador_vegadeswhile+=1
