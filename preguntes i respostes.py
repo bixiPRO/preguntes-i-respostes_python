10a36fe1cfd68d9c2b04c3124e8c1a836d6912ce
Bixiang <bixiangzhu6666@gmail.com>
2024-01-06 15:52:30 +0100

    canvis nom de variables

--- preguntes i respostes.py
+++ preguntes i respostes.py
@@ -1,5 +1,5 @@
-import random
 #PRE://Ha de mostrar una resposta aleatori i que l'usuari ha de respondre la pregunta
+import random
 #llista de preguntes amb la seva resposta
 llista_preguntes=[("Quina es la capital de espana","Madrid"),("Quina es la capital de italia", "Roma")]
 
@@ -11,8 +11,8 @@ while resposta_bucle == "si":
     #numero aleatori
     seed = random.randint(0, 100)
     seed = (seed * 997) % 1000
-    random = (seed * 503) % 1000 / 100
-    numero_pregunta = int(random * (len(llista_preguntes)))
+    random_numero = (seed * 503) % 1000 / 100
+    numero_pregunta = int(random_numero * (len(llista_preguntes)))
 
     #mostrar pregunta
     pregunta_elegir=llista_preguntes[numero_pregunta%len(llista_preguntes)]
@@ -29,10 +29,10 @@ while resposta_bucle == "si":
         print("Resposta correcte! +1 punt")
     else:
         print("Resposta incorrecte!")
-
-    usuari=input("Vols continuar jugant? (si/no)")
-    usuari=usuari.strip()
-    usuari=usuari.lower()
+    #preguntar l'usuari si vol continuar jugant
+    resposta_bucle=input("Vols continuar jugant? (si/no)")
+    resposta_bucle=resposta_bucle.strip()
+    resposta_bucle=resposta_bucle.lower()
 
 print(f"Puntuació final: {puntuacio}")
 #POST://Mostra la puntuació de l'usuari d'acord de com ha respost la pregunta
